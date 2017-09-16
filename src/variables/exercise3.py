ee9a11e69f9f658ee62c5e0e08c584b0d6bf32cd
let42 <alberto@gucciardi.xyz>
2017-09-17 00:44:50 +0200

    Calculate the modulus of the values typed and print it

---
+++ src/variables/exercise3.py
@@ -0,0 +1,7 @@
+# Calculate the modulus of the values typed and print it
+
+input1 = int(raw_input('Type a number here >>> '))
+input2 = int(raw_input('Type a number here >>> '))
+res = input1 % input2
+print 'The modulus between ', input1, ' and ', input2, ' is ', res, '.'
+quit()
