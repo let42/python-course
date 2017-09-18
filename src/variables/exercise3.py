c7f237e4b38f4f311bac44136b693f0b8d56f23b
let42 <alberto@gucciardi.xyz>
2017-09-18 19:17:43 +0200

    eliminate useless parts

--- src/variables/exercise3.py
+++ src/variables/exercise3.py
@@ -1,7 +1,12 @@
-# Calculate the modulus of the values typed and print it
+# Exercise 3 Write a program to prompt the user for hours and rate per hour to compute gross
 
-input1 = int(raw_input('Type a number here >>> '))
-input2 = int(raw_input('Type a number here >>> '))
-res = input1 % input2
-print 'The modulus between ', input1, ' and ', input2, ' is ', res, '.'
+hours = int(raw_input('Inser the number of hours which have you worked >>>'))
+payrate = float(raw_input('Insert your pay rate >>>'))
+
+gross_pay = (payrate * hours)
+net_pay = (payrate * hours) * 0.75
+
+print 'Gross pay: ', gross_pay, '\n'
+print 'Net pay: ', net_pay, '\n'
+
 quit()
