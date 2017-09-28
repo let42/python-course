1c2026c90a6b13704a8283376ab46d45285e6038
let42 <alberto@gucciardi.xyz>
2017-09-28 15:55:05 +0200

    # Rewrite your pay program using try and except so that your program handles non-numeric input gracefully by # printing a message and exiting the program. The following shows two executions of the program

--- src/conditionals/exercise5.py
+++ src/conditionals/exercise5.py
@@ -1,5 +1,5 @@
-#
-#
+# Rewrite your pay program using try and except so that your program handles non-numeric input gracefully by
+# printing a message and exiting the program. The following shows two executions of the program
 
 hours = raw_input('Enter Hours: ')
 try:
